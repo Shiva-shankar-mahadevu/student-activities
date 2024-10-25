8da258cc9e05b9be5439f786867dcc11b52ab9ca
KarunyaReddyJ <KarunyaReddy1418@gmail.com>
2024-10-25 10:57:22 +0530

    created db connection and created student model

---
+++ backend/database/model/user.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose');
+
+ 
+// Define the  Student Schema
+const studentSchema = new mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true, // Removes extra whitespace
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6, // Ensures password has a minimum length
+  },
+}, {
+  timestamps: true // Adds createdAt and updatedAt timestamps
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports=Student;
